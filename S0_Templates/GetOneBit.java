ead12e2d47f30ac8b2378d945c73c42c58fed63d
silentwings2004 <silentwings2004@hotmail.com>
2021-08-30 21:44:31 -0700

    update template

---
+++ S0_Templates/GetOneBit.java
@@ -0,0 +1,12 @@
+package S0_Templates;
+
+public class GetOneBit {
+    public int getOneBit(int state) {
+        int count = 0;
+        while (state > 0) {
+            count++;
+            state = state & (state - 1);
+        }
+        return count;
+    }
+}
