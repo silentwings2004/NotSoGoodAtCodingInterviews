783c16609b4f7a92a8eeba3991e1966ab165671d
silentwings2004 <silentwings2004@hotmail.com>
2023-01-09 13:23:48 -0800

    update the repo

--- S0_Templates/IntArrayToList.java
+++
@@ -1,9 +0,0 @@
-package S0_Templates;
-import java.util.*;
-public class IntArrayToList {
-    int N = (int)(1e9 + 7);
-    public List<Integer> intArrayToList() {
-        Integer[] res = new Integer[N];
-        return Arrays.asList(res);
-    }
-}
